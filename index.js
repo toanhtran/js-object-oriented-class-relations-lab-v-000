5f9646a12385e860efeeb0af6775f8eda01eb190
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-13 03:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,3 +38,21 @@ class Passenger {
     });
   }
 }
+
+class Trip {
+  constructor(driver, passenger){
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
+  }
+  driver(){
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
+}
