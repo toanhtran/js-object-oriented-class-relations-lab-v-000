4ed12c5ecd2c838cef3c7f3a6f5d81d1b0427d0c
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-13 02:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ let store = {drivers: [], passengers: [], trips: []};
 let driverId = 0;
 let passengerId = 0;
 let tripId = 0;
+
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
+  }
+}
