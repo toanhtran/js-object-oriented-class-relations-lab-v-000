5fa1229c37a3243018e4fe3caec1c765655008bb
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-13 03:03:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,50 +9,5 @@ constructor(name) {
     this.id = driverId++;
     store.drivers.push(this);
   }
-  trips() {
-    return store.trips.filter(trip => {
-      return trip.driverId == this.id;
-    });
-  }
-  passengers() {
-     return this.trips().map(trip => {
-       return trip.passenger();
-     });
-   }
- }
-  class Passenger {
-    constructor(name) {
-      this.name = name;
-      this.id = passengerId++;
-      store.passenger.push(this);
-    }
-    
-    trips() {
-      return store.trips.filter(trip => {
-        return trip.passengerId == this.id;
-      });
-    }
-    drivers(){
-      return.this.trips().map(trip => {
-        return trip.drivers();
-      });
-    }
-  }
-  class Trip {
-    constructor(driver, passenger) {
-      this.driverId = driver.id;
-      this.passengerId = passenger.id;
-      this.id = tripId++;
-      store.trips.push(this);
-  }
-  driver() {
-   return store.drivers.find(driver => {
-     return driver.id === this.driverId;
-   });
- }
- passenger() {
-   return store.passengers.find(passenger => {
-     return passenger.id === this.passengerId;
-   });
- }
+
 }
